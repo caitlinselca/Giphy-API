d59019cf549d804058a267445a0b0b8d63ccb70b
Caitin Selca <caitlinselca@gmail.com>
2020-01-09 22:26:46 -0500

    Finished everything. Fixed the design.

--- src/SearchField.js
+++ src/SearchField.js
@@ -1,53 +1,95 @@
 import React from "react";
 import axios from "axios";
 import GifList from "./GifList";
+import GifItem from "./GifItem";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          term: '',
-          gifs: [],
-          isError: false
-        };
-      }
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: "",
+            gifs: [],
+            rating: "",
+			isError: false,
+			pageNumber: 0,
+			itemsPerPage: 15
+		};
+	}
 
-      componentDidMount(){
-        axios.get(
-            'http://api.giphy.com/v1/gifs/trending?api_key=mLxD4Hc77uYnY3b5VTGOm64fNtmh3CIA'
-        )
-          .then(trendinggifs => {
-            this.setState({
-              gifs: trendinggifs.data.data
-            })
-          })
-          .catch(err => {
-            this.setState({ isError: true, gifs: [] });
-          });
-      }
+	componentDidMount() {
+		axios
+			.get(
+				"http://api.giphy.com/v1/gifs/trending?api_key=mLxD4Hc77uYnY3b5VTGOm64fNtmh3CIA",
+				{
+					params: {
+						limit: this.state.itemsPerPage,
+						offset: this.state.pageNumber * this.state.itemsPerPage
+					}
+				}
+			)
+			.then(trendinggifs => {
+				this.setState({
+					gifs: trendinggifs.data.data,
+					pageNumber: this.state.pageNumber + 1
+				});
+			})
+			.catch(err => {
+				this.setState({ isError: true, gifs: [] });
+			});
+	}
 
 	handleChange = e => {
 		this.setState({
 			term: e.target.value
 		});
-	};
+    };
+    
+    handleRandomChange = j => {
+		this.setState({
+			rating: j.target.value
+		});
+    };
 
 	handleSubmit = e => {
 		axios
 			.get(
-				`http://api.giphy.com/v1/gifs/search?q=${this.state.term.toUpperCase()}&api_key=mLxD4Hc77uYnY3b5VTGOm64fNtmh3CIA`
+				`http://api.giphy.com/v1/gifs/search?q=${this.state.term.toUpperCase()}&rating=${this.state.rating}&api_key=mLxD4Hc77uYnY3b5VTGOm64fNtmh3CIA`
 			)
 			.then(res => {
 				let data = res.data.data.map(image => {
-                    return {
-                        id: image.id,
-                        embed_url: image.embed_url
-                    };
-                });
-				this.setState({gifs: data, isError: false});
+					return {
+						id: image.id,
+                        embed_url: image.embed_url,
+                        rating: image.rating
+					};
+				});
+				this.setState({ gifs: data, isError: false });
 			})
 			.catch(e => {
-                this.setState({ isError: true, gifs: [] });
+				this.setState({ isError: true, gifs: [] });
+			});
+	};
+
+	handleBottom = e => {
+		axios
+			.get(
+				"http://api.giphy.com/v1/gifs/trending?api_key=mLxD4Hc77uYnY3b5VTGOm64fNtmh3CIA",
+				{
+					params: {
+						limit: this.state.itemsPerPage,
+						offset: this.state.pageNumber * this.state.itemsPerPage
+					}
+				}
+			)
+			.then(trendinggifs => {
+				this.setState({
+					gifs: this.state.gifs.concat(trendinggifs.data.data),
+					pageNumber: this.state.pageNumber + 1
+				});
+			})
+			.catch(err => {
+				this.setState({ isError: true, gifs: [] });
 			});
 	};
 
@@ -65,6 +107,13 @@ class SearchBar extends React.Component {
 						className="input-field"
 						onChange={this.handleChange}
 					></input>
+                     <select name="rating" class="filter-by-rating" onChange={this.handleRandomChange} >
+                        <option value="" select></option>
+                        <option value="g" select>G</option>
+                        <option value="pg" select>PG</option>
+                        <option value="pg-13" select>PG-13</option>
+                        <option value="r" select>R</option>
+                    </select>
 					<button
 						className="search-button"
 						onClick={this.handleSubmit}
@@ -72,7 +121,19 @@ class SearchBar extends React.Component {
 						Search
 					</button>
 				</div>
-                <GifList gifs={this.state.gifs}></GifList>
+				{console.log(this.state.pageNumber, this.state.itemsPerPage)}
+				<InfiniteScroll
+					dataLength={this.state.gifs.length}
+					next={this.handleBottom}
+					hasMore={true}
+					loader={<h4>Loading...</h4>}
+				>
+					<div className="gif-list">
+						{this.state.gifs.map(image => {
+							return <GifItem gif={image} />;
+						})}
+					</div>
+				</InfiniteScroll>
 			</div>
 		);
 	}
